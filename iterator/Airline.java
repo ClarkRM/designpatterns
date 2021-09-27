8210f544d8ded31911330f18914cd270c5504e34
ClarkRM <robbie.0124@gmail.com>
2021-09-26 21:51:16 -0400

    adding airline class

--- iterator/Airline.java
+++ iterator/Airline.java
@@ -8,10 +8,21 @@ public class Airline {
 
     public Airline(String title) {
         this.title.equals(title);
+        flights = new Flight[1];
+        size = flights.length;
     }
 
     public void addFlight(String flightNum, String from, String to, int duration, int transfers) {
-
+        Flight flight = new Flight(flightNum, from, to, duration, transfers);
+        if(flights[size] != null) {
+            flights = growArray(flights).clone();
+            flights[size] = flight;
+        }
+        for(int i = 0; i < size; i++) {
+            if(flights[i] == null) {
+                flights[i] = flight;
+            }
+        }
     }
 
     public String getTitle() {
@@ -19,12 +30,14 @@ public class Airline {
     }
 
     private Flight[] growArray(Flight[] flights) {
-        
-        return flights;
+        size++;
+        Flight[] largerFlights = new Flight[size];
+        largerFlights = flights.clone();
+        return largerFlights;
     }
 
     public FlightIterator createIterator() {
-        return null;
+        return new FlightIterator(flights);
     }
     
 }
--- iterator/FlightIterator.java
+++ iterator/FlightIterator.java
@@ -4,18 +4,20 @@ import java.util.Iterator;
 public class FlightIterator {
 
     private Flight[] flights;
-    private int position;
+    private int position = 0;
 
     public FlightIterator(Flight[] flights) {
-
+            this.flights = flights;
     } 
 
     public boolean hasNext() {
+        if(position >= flights.length || flights[position] == null)
+            return false;
         return true;
     }
 
     public Flight next() {
-        return null;
+        return flights[position++];
     }
     
 }
