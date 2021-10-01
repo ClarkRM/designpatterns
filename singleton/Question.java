d39cab70641e0a7e82b9353c59f785551a3275b2
ClarkRM <robbie.0124@gmail.com>
2021-10-01 15:46:17 -0400

    Setting up singleton folder and Question object

---
+++ singleton/Question.java
@@ -0,0 +1,34 @@
+package singleton;
+
+public class Question {
+    
+    private String question;
+    private String[] answers;
+    private int correctAnswer;
+
+    public Question(String question, String ans1, String ans2, String ans3, String ans4, int correctAnswer) {
+        this.question = question;
+        this.answers[0] = ans1;
+        this.answers[1] = ans2;
+        this.answers[2] = ans3;
+        this.answers[3] = ans4;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String toString() {
+
+        return this.question + "\n1: "+ this.answers[0]
+            + "\n2: "+ this.answers[1] + "\n3: "+ this.answers[2]
+            + "\n4: "+ this.answers[3] + "\nEnter answer: ";
+    }
+
+    public boolean isCorrect(int userAnswer) {
+
+        return userAnswer == this.correctAnswer;
+    }
+
+    public String getCorrectAnswer() {
+
+        return "You got it right! The correct answer was: "+this.answers[correctAnswer];
+    }
+}
