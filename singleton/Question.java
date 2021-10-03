4c927129d2b5123265f3f2c49e810b73df54b504
ClarkRM <robbie.0124@gmail.com>
2021-10-02 21:39:40 -0400

    Adding javadoc and standardizing style

--- singleton/Question.java
+++ singleton/Question.java
@@ -1,12 +1,27 @@
 package singleton;
 
+/**
+ * A class to create the trivia question object
+ * @author Robbie Clark
+ */
 public class Question {
     
     private String question;
     private String[] answers;
     private int correctAnswer;
 
+    /**
+     * Constructor, initializes the question, array of answers, and the number value corresponding 
+     * to the correct answer
+     * @param question The trivia question being asked
+     * @param ans1 The first answer option
+     * @param ans2 The second answer option
+     * @param ans3 The third answer option
+     * @param ans4 The fourth answer option
+     * @param correctAnswer the number corresponding to the correct answer
+     */
     public Question(String question, String ans1, String ans2, String ans3, String ans4, int correctAnswer) {
+        
         this.answers = new String[4];
         this.question = question;
         this.answers[0] = ans1;
@@ -16,6 +31,9 @@ public class Question {
         this.correctAnswer = correctAnswer;
     }
 
+    /**
+     * A method to display the question and answer options as a String
+     */
     public String toString() {
 
         return this.question + "\n1: "+ this.answers[0]
@@ -23,11 +41,20 @@ public class Question {
             + "\n4: "+ this.answers[3] + "\n\nEnter answer: ";
     }
 
+    /**
+     * A method that checks to see if the user entered the correct answer
+     * @param userAnswer the integer value of the correct answer, 1 larger than the correctAnswer variable
+     * @return true if the user entered the right value, false if the user entered the wrong value
+     */
     public boolean isCorrect(int userAnswer) {
 
         return userAnswer == this.correctAnswer+1;
     }
 
+    /**
+     * A method to display which answer was correct
+     * @return A string stating which which answer was correct
+     */
     public String getCorrectAnswer() {
 
         return "The correct answer was: "+this.answers[correctAnswer];
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -3,7 +3,10 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-
+/**
+ * A class that handles the Singleton setup of the game
+ * @author Robbie Clark
+ */
 public class TriviaGame {
 
     private static TriviaGame triviaGame;
@@ -12,7 +15,12 @@ public class TriviaGame {
     private Scanner reader;
     private ArrayList<Question> questions;
 
+    /**
+     * Private constructor that initializes the score, Random class, and Scanner class, as well as 
+     * getting the quesitons list from the DataLoader and starting the game
+     */
     private TriviaGame() {
+       
         this.questions = DataLoader.getTriviaQuestions();
         this.score = 0;
         rand = new Random();
@@ -20,11 +28,20 @@ public class TriviaGame {
         playRound();
     }
 
+    /**
+     * The static method that initializes the entire game
+     * @return The game instance
+     */
     public static TriviaGame getInstance() {
+        
         return new TriviaGame();
     }
 
+    /**
+     * The method that handles replaying and prints out the user's score if they quit
+     */
     public void play() {
+        
         boolean playResponse = true;
         while(playResponse) {
             
@@ -42,6 +59,12 @@ public class TriviaGame {
         System.out.println("\nYou won "+ this.score + " games!\nGoodbye");
     }
 
+    /**
+     * The method that prints a random question and accepts the User's guess, increasing the score by
+     * 1 if the user got the question correct, printing an error message if the user puts in an
+     * invalid guess, and printing the correct answer if the user's input was invalid or wrong
+     * @return true if the guess was correct, false if the guess was invalid or incorrect
+     */
     private boolean playRound() {
         
         int questionNum = rand.nextInt(10);
@@ -62,6 +85,5 @@ public class TriviaGame {
         System.out.println("You got it wrong!");
         System.out.println(this.questions.get(questionNum).getCorrectAnswer());
         return false;
-    }
-    
+    }  
 }
