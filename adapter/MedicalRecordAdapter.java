1f10093b7865c503455b41c27aa16e3b65588281
ClarkRM <robbie.0124@gmail.com>
2021-12-03 17:38:45 -0500

    added output.png

--- adapter/MedicalRecordAdapter.java
+++ adapter/MedicalRecordAdapter.java
@@ -5,29 +5,49 @@ import java.util.ArrayList;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 
+/**
+ * A class to convert HealthRecords to the MedicalRecord format
+ * @author Robbie Clark
+ */
 public class MedicalRecordAdapter implements MedicalRecord {
     private HealthRecord record;
 
+    /**
+     * Constructor for the adapter, takes in a HealthRecord to adapt to a Medical Record
+     * @param record
+     */
     public MedicalRecordAdapter(HealthRecord record) {
         this.record = record;
     }
     
+    /**
+     * A method that takes the full name and parses it into two, returning the first name
+     */
     public String getFirstName() {
         String Name = this.record.getName();
         String[] retArray = Name.split(" ");
         return retArray[0];
     }
 
+    /**
+     * A method that takes the full name and parses it into two, returning the last name
+     */
     public String getLastName() {
         String Name = this.record.getName();
         String[] retArray = Name.split(" ");
         return retArray[1];
     }
 
+    /**
+     * A method that returns the patient's birthdate as a Date object
+     */
     public Date getBirthday() {
         return this.record.getBirthdate();
     }
 
+    /**
+     * A method that takes in the String gender fron the HealthRecord and interprets it into a Gender enum
+     */
     public Gender getGender() {
         String gender = this.record.getGender();
         if(gender.equalsIgnoreCase("male")) {return Gender.MALE;}
@@ -35,10 +55,17 @@ public class MedicalRecordAdapter implements MedicalRecord {
         else {return Gender.OTHER;}
     }
 
+    /**
+     * A method that adds a new visit into the HealthRecord's ArrayList, so that visits stay in chronological order
+     */
     public void addVisit(Date date, boolean well, String description) {
        this.record.addHistory(date, well, description);
     }
 
+    /**
+     * A method that takes each String from the HealthRecord arraylist and parses it into the components for a Visit object
+     * Then adds the info into an arraylist of Visits and returns that arraylist
+     */
     public ArrayList<Visit> getVisitHistory() {
         ArrayList<String> oldHistory = this.record.getHistory();
         ArrayList<Visit> newHistory = new ArrayList<Visit>();
@@ -49,7 +76,7 @@ public class MedicalRecordAdapter implements MedicalRecord {
             // line 1- Well visit: ...
             // line 2- comments
             
-            String[] retArray1 = oldVisitSplit[0].split(" "); //split the day of the week from the actual number date
+            String[] retArray1 = oldVisitSplit[0].split(" ");
             String sDay = retArray1[2].substring(0,2);
             String sMonth = retArray1[3].substring(0,2);
             int day = Integer.parseInt(sDay);
@@ -73,6 +100,10 @@ public class MedicalRecordAdapter implements MedicalRecord {
     }
 
 
+    /**
+     * toString, converts Gender enum into a regualar String with first letter capitalized, converts date to a simplified format,
+     * and returns all the patient's information in the same format as a MedicalRecord
+     */
     public String toString() {
         ArrayList<Visit> history = getVisitHistory();
         Gender gender = getGender();
